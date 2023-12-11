0db59008dc816a3987ca0546b2287c227b69339e
Pahlevi Fikri Auliya <levi@ruangguru.com>
2023-12-11 22:03:06 +0700

    refactor: split into multiple routes

---
+++ app/dependencies.py
@@ -0,0 +1,17 @@
+import os
+from typing import Annotated, Union
+from clients.openai_client import OpenAIClient
+from ai import AI
+
+
+
+def init_ai():
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    oai_client = OpenAIClient(OPENAI_API_KEY)
+    return AI(oai_client, "You are a chat assistant. Reply briefly, 2 sentences max")
+
+
+def random_id():
+    import random
+    return random.randint(0, 1 << 32)
+
--- app/routers/threads.py
+++ app/routers/threads.py
@@ -1,35 +1,13 @@
-import os
-from typing import Annotated, Union
-from dotenv import load_dotenv
-
-from fastapi import FastAPI, Form, Request
+from fastapi import APIRouter
+from fastapi import Form, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from clients.openai_client import OpenAIClient
-from ai import AI
-from chatapp import ChatApp
+from app.dependencies import init_ai, random_id
 from entities import Chat, User
+from chatapp import ChatApp
 
-
-app = FastAPI()
-app.mount("/public", StaticFiles(directory="public"), name="public")
+router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-
-load_dotenv()
-
-
-def init_ai():
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    oai_client = OpenAIClient(OPENAI_API_KEY)
-    return AI(oai_client, "You are a chat assistant. Reply briefly, 2 sentences max")
-
-
-def random_id():
-    import random
-    return random.randint(0, 1 << 32)
-
 
 ai = init_ai()
 chat_app = ChatApp()
@@ -37,7 +15,7 @@ chat_app.add_thread(0, "main", Chat(
     1, User(random_id(), "assistant"), "Hello, I am your virtual assistant. Ask me anything"))
 
 
-@app.get("/", response_class=HTMLResponse)
+@router.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     thread = chat_app.get_thread(0)
     audio_path = f"/public/{thread.chats[-1].id}.mp3"
@@ -45,7 +23,7 @@ async def read_root(request: Request):
                                                       "audio_path": audio_path})
 
 
-@app.post("/chat", response_class=HTMLResponse)
+@router.post("/chat", response_class=HTMLResponse)
 async def chat(request: Request, message: str = Form(...)):
     new_chat = Chat(random_id(), User(1, "user"), message)
     chat_app.add_chat(0, "main", new_chat)
